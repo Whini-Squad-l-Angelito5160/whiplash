769f088912f6f5d1680db9b1dd9a89f574165b7e
Whini <103304451+Whini-Squad-l-Angelito5160@users.noreply.github.com>
2022-06-01 16:34:49 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,54 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "jugador":
+            case "anim1":return [img`
+. . . . d d d d d d . . . . . . 
+. . . . d f d d f d . . . . . . 
+. . . . d d d d d d . . . . . . 
+. . . . 5 d f f d 5 . . . . . . 
+. . 5 5 5 5 5 5 5 5 5 5 . . . . 
+. . 5 5 5 5 5 5 5 5 5 5 . . . . 
+. . d d 5 5 5 5 5 5 d d . . . . 
+. . d d 5 5 5 5 5 5 d d . . . . 
+. . d d 5 5 5 5 5 5 d d . . . . 
+. . . . f f f f f f . . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . f f f . . f f . . . . . . 
+. . . f f . . . f f . . . . . . 
+. 5 f f . . . . f f . . . . . . 
+. 5 f . . . . . f f . . . . . . 
+. 5 . . . . . . 5 5 5 . . . . . 
+`, img`
+. . d d d d d d d . . . . . . . 
+. . d f d d d f d . . . . . . . 
+. . d d d d d d d . . . . . . . 
+. . 5 d f f f d 5 . . . . . . . 
+5 5 5 5 5 5 5 5 5 5 5 . . . . . 
+5 5 5 5 5 5 5 5 5 5 5 . . . . . 
+d d 5 5 5 5 5 5 5 d d . . . . . 
+d d 5 5 5 5 5 5 5 d d . . . . . 
+d d 5 5 5 5 5 5 5 d d . . . . . 
+. . f f f f f f f . . . . . . . 
+. . f f f f f f f . . . . . . . 
+. . f f . . . f f f . . . . . . 
+. . f f . . . . f f f . . . . . 
+. . f f . . . . . . f f . . . . 
+. . f f . . . . . . f f . . . . 
+. . 5 5 5 . . . . . 5 5 5 . . . 
+`];
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
